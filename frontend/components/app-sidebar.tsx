b0a6b7700a985c452007aed767a02ecbd63c38ec
JoySarkarBD <developer.joysarkar@gmail.com>
2026-01-16 13:44:23 +0600

    refactor: simplify AppSidebar structure and improve readability

--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -32,51 +32,49 @@ const clients = [
 
 export function AppSidebar() {
   return (
-    <div className='h-full sticky top-0 z-20'>
-      <Sidebar collapsible='icon' className='h-screen flex flex-col'>
-        <SmartToggle />
-        <SidebarContent className='flex-1'>
-          <SidebarGroup>
-            <SidebarMenu>
-              {/* Collapsible client list */}
-              <Collapsible defaultOpen className='group/collapsible'>
-                <SidebarMenuItem>
-                  <CollapsibleTrigger asChild>
-                    <SidebarMenuButton>
-                      <Building2 className='h-4 w-4' />
-                      <span>Company Management</span>
-                      <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180 h-4 w-4' />
-                    </SidebarMenuButton>
-                  </CollapsibleTrigger>
-                </SidebarMenuItem>
-                <CollapsibleContent>
-                  <SidebarMenuSub>
-                    {clients.map((client) => (
-                      <SidebarMenuSubItem key={client.name}>
-                        <SidebarMenuSubButton asChild>
-                          <Link href={`/clients/${client.id}`}>
-                            {client.name}
-                          </Link>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                </CollapsibleContent>
-              </Collapsible>
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter className='p-4'>
-          <SidebarMenuButton
-            asChild
-            className='text-destructive hover:bg-destructive/10'>
-            <div className='flex items-center gap-2'>
-              <LogOut className='h-4 w-4' />
-              <span>Logout Account</span>
-            </div>
-          </SidebarMenuButton>
-        </SidebarFooter>
-      </Sidebar>
-    </div>
+    <Sidebar collapsible='icon' className='h-screen flex flex-col'>
+      <SmartToggle />
+      <SidebarContent className='flex-1'>
+        <SidebarGroup>
+          <SidebarMenu>
+            {/* Collapsible client list */}
+            <Collapsible defaultOpen className='group/collapsible'>
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton>
+                    <Building2 className='h-4 w-4' />
+                    <span>Company Management</span>
+                    <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180 h-4 w-4' />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+              </SidebarMenuItem>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {clients.map((client) => (
+                    <SidebarMenuSubItem key={client.name}>
+                      <SidebarMenuSubButton asChild>
+                        <Link href={`/clients/${client.id}`}>
+                          {client.name}
+                        </Link>
+                      </SidebarMenuSubButton>
+                    </SidebarMenuSubItem>
+                  ))}
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </Collapsible>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter className='p-4'>
+        <SidebarMenuButton
+          asChild
+          className='text-destructive hover:bg-destructive/10'>
+          <div className='flex items-center gap-2'>
+            <LogOut className='h-4 w-4' />
+            <span>Logout Account</span>
+          </div>
+        </SidebarMenuButton>
+      </SidebarFooter>
+    </Sidebar>
   );
 }
