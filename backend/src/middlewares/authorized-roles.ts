2af49f891276892244d0fa09232f058be4ce4fc3
JoySarkarBD <developer.joysarkar@gmail.com>
2026-01-23 09:39:11 +0600

    feat: add authorized-roles middleware implementation

---
+++ backend/src/middlewares/authorized-roles.ts
@@ -0,0 +1,2 @@
+// here will be the authorized-roles middleware implementation
+// it will take an array of roles and check if the user has one of the roles (e.g, authorizedRoles(['admin', 'editor']))
