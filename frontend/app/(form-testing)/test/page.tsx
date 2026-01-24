ef6cbfa231d52a657f11f888c28f5032215dac83
rakib3719 <rakib3719>
2026-01-24 12:21:20 +0600

    initialize keycloak

--- frontend/app/(form-testing)/test/page.tsx
+++ frontend/app/(form-testing)/test/page.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/alert-dialog";
 import { registerSchema } from "@/components/universal-form/form.schema";
 import UniversalForm from "@/components/universal-form/UniversalForm";
+import { useKeycloak } from "@/hooks/useKeycloak";
 import z from "zod";
 import { registerFields } from "./testFormsFIeld";
 
@@ -19,23 +20,26 @@ export default function RegisterPage() {
     console.log("Form submitted:", data);
   };
 
+  const keyCloak = useKeycloak();
+  console.log(keyCloak, "ami keyclaok");
+
   return (
-    <div className='max-w-md mx-auto mt-10'>
+    <div className="mx-auto mt-10 max-w-md">
       <AlertDialog>
-        <AlertDialogTitle className='hidden'></AlertDialogTitle>
+        <AlertDialogTitle className="hidden"></AlertDialogTitle>
         <AlertDialogTrigger asChild>
-          <button className='bg-primary  text-white font-bold py-2 px-4 rounded'>
+          <button className="bg-primary rounded px-4 py-2 font-bold text-white">
             Show Form
           </button>
         </AlertDialogTrigger>
 
-        <AlertDialogContent className='px-0 overflow-y-scroll max-h-[80vh] rounded-none'>
+        <AlertDialogContent className="max-h-[80vh] overflow-y-scroll rounded-none px-0">
           <UniversalForm<RegisterFormData>
-            title='Add New Entry'
+            title="Add New Entry"
             fields={registerFields}
             schema={registerSchema}
             onSubmit={handleRegister}
-            submitText='Register'
+            submitText="Register"
           />
 
           <AlertDialogFooter></AlertDialogFooter>
