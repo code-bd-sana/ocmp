a926fa3d7d5bf3bda6d621f943f7c00629d5368d
JoySarkarBD <developer.joysarkar@gmail.com>
2026-02-10 14:05:04 +0600

    feat: add SubscriptionCoupon model and schema for coupon management

---
+++ backend/src/models/subscription-billing/subscriptionCoupon.schema.ts
@@ -0,0 +1,53 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+// Define and export an interface representing a SubscriptionCoupon document
+export interface ISubscriptionCoupon extends Document {
+  code: string;
+  isActive: boolean;
+  usedBy: mongoose.Types.ObjectId[]; // Array of user IDs who have used the coupon
+  users: mongoose.Types.ObjectId[]; // Array of user IDs who can use the coupon
+  createdBy: mongoose.Types.ObjectId;
+}
+
+// Define the SubscriptionCoupon schema
+const SubscriptionCouponSchema: Schema<ISubscriptionCoupon> = new Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      uppercase: true,
+    }, // Unique coupon code
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: true,
+    }, // Indicates if the coupon is active and can be used
+    usedBy: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User', // Reference to the User model
+      },
+    ], // Array of user IDs who have used the coupon
+    users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User', // Reference to the User model
+      },
+    ], // Array of user IDs who can use the coupon
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User', // Reference to the User model
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+// Create the SubscriptionCoupon model
+const SubscriptionCoupon = mongoose.model<ISubscriptionCoupon>(
+  'SubscriptionCoupon',
+  SubscriptionCouponSchema
+);
+
+// Export the SubscriptionCoupon model
+export default SubscriptionCoupon;
