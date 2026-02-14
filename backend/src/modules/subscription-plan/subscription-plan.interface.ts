68b9b34c66704133da56bc41e78c3491a6c36b0a
rakib3719 <rakib3719>
2026-02-14 15:32:34 +0600

    feat:subscription duration

--- backend/src/modules/subscription-plan/subscription-plan.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Type definition for subscription-plan.
- *
- * This type defines the structure of a single subscription-plan object.
- * @interface TSubscriptionPlan
- */
-export interface TSubscriptionPlan {
-  // Add fields as needed
-}
